0c34e4050277644e20c5d413075ccb6cda3966e2
李茂同 <li.maotong@simuladesign.com>
2017-06-05 09:58:04 +0800

    SQLite使用demo

---
+++ SQLiteDemo/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Diagnostics;
+
+namespace SQLiteDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitLog();
+            ImportData2Db();
+            Console.ReadKey();
+        }
+
+        static void InitLog()
+        {
+            Trace.AutoFlush = true;
+            var consoleListener = new ConsoleTraceListener();
+            Trace.Listeners.Add(consoleListener);
+        }
+
+        static void ImportData2Db()
+        {
+            var items = GetTestData();
+            try
+            {
+                var dbPath = System.Environment.CurrentDirectory + "\\DLL\\EventLogDb.db3";
+                Trace.TraceInformation("EventLogDb.db3 Path:"+ dbPath);
+                using (var conn = new SQLiteConnection("Data Source="+dbPath))
+                {
+                    conn.Open();
+                    foreach (SqlEnvent e in items)
+                    {
+                        SqlEnventOper.UpdateEvent2Table(conn, e);
+                    }
+                }
+                Trace.TraceInformation("操作数据库成功！");
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError("操作数据库失败：{0}", ex.Message);
+            }
+        }
+
+        static IEnumerable<SqlEnvent> GetTestData()
+        {
+            var items = new List<SqlEnvent>();
+            items.Add(new SqlEnvent()
+            {
+                ID = "0001",
+                LoginAccount = "张三",
+                Type = "Login",
+                TimeStamp = DateTime.Now
+            });
+            items.Add(new SqlEnvent()
+            {
+                ID = "0002",
+                LoginAccount = "李四",
+                Type = "Login",
+                TimeStamp = DateTime.Now
+            });
+            items.Add(new SqlEnvent()
+            {
+                ID = "0003",
+                LoginAccount = "王五",
+                Type = "Login",
+                TimeStamp = DateTime.Now
+            });
+            return items;
+        }
+    }
+}
---
+++ SQLiteDemo/SQLiteOperation.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace SQLiteDemo
+{
+    internal class SqlEnvent
+    {
+        public string ID { get; set; }
+        public string Type { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string LoginAccount { get; set; }
+    }
+    internal class SqlEnventOper
+    {
+        public static bool AddEvent2Table(SQLiteConnection conn,SqlEnvent lEvent)
+        {
+            try
+            {
+                    SQLiteCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = "insert into Events(ID,Type,TimeStamp,LoginAccount) values(@ID,@Type,@TimeStamp,@LoginAccount);";
+                    cmd.Parameters.Add(new SQLiteParameter("ID", lEvent.ID));
+                    cmd.Parameters.Add(new SQLiteParameter("Type", lEvent.Type));
+                    cmd.Parameters.Add(new SQLiteParameter("TimeStamp", lEvent.TimeStamp));
+                    cmd.Parameters.Add(new SQLiteParameter("LoginAccount", lEvent.LoginAccount));
+                    int i = cmd.ExecuteNonQuery();
+                    return i == 1;
+            }
+            catch (Exception)
+            {
+                //Do any logging operation here if necessary
+                return false;
+            }
+        }
+        public static bool DeleteEventFromTable(SQLiteConnection conn, DateTime timeStamp)
+        {
+            try
+            {         
+                    SQLiteCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = "delete from Events where TimeStamp<@TimeStamp;";
+                    cmd.Parameters.Add(new SQLiteParameter("TimeStamp", timeStamp));
+                    int i = cmd.ExecuteNonQuery();
+                    return i == 1;
+            }
+            catch (Exception)
+            {
+                //Do any logging operation here if necessary
+                return false;
+            }
+        }
+        public static List<SqlEnvent> GetEventByTime(SQLiteConnection conn, DateTime timeStamp)
+        {
+            try
+            {
+                    SQLiteCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = "select * from Events where TimeStamp>=@TimeStamp;";
+                    cmd.Parameters.Add(new SQLiteParameter("TimeStamp", timeStamp));
+                    SQLiteDataReader dr = cmd.ExecuteReader();
+                    if(dr.HasRows)
+                    {
+                        var eventList = new List<SqlEnvent>();
+                        while (dr.Read())
+                        {
+                            var lEvent = new SqlEnvent();
+                            lEvent.ID = dr.GetString(1);
+                            lEvent.Type = dr.GetString(2);
+                            lEvent.TimeStamp = dr.GetDateTime(3);
+                            lEvent.LoginAccount = dr.GetString(0);
+                            eventList.Add(lEvent);                           
+                        }
+                        return eventList;
+                    }  
+                    return null;
+            }
+            catch (Exception)
+            {
+                //Do any logging operation here if necessary
+                return null;
+            }
+        }
+        public static bool UpdateEvent2Table(SQLiteConnection conn,SqlEnvent lEvent)
+        {
+            try
+            {
+                SQLiteCommand cmdU = conn.CreateCommand();
+                cmdU.CommandText = "update Events set ID=@ID,Type=@Type,TimeStamp=@TimeStamp where LoginAccount=@LoginAccount;";
+                cmdU.Parameters.Add(new SQLiteParameter("ID", lEvent.ID));
+                cmdU.Parameters.Add(new SQLiteParameter("Type", lEvent.Type));
+                cmdU.Parameters.Add(new SQLiteParameter("TimeStamp", lEvent.TimeStamp));
+                cmdU.Parameters.Add(new SQLiteParameter("LoginAccount", lEvent.LoginAccount));
+                int i = cmdU.ExecuteNonQuery();
+                if (i == 0)
+                {
+                    SQLiteCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = "insert into Events(ID,Type,TimeStamp,LoginAccount) values(@ID,@Type,@TimeStamp,@LoginAccount);";
+                    cmd.Parameters.Add(new SQLiteParameter("ID", lEvent.ID));
+                    cmd.Parameters.Add(new SQLiteParameter("Type", lEvent.Type));
+                    cmd.Parameters.Add(new SQLiteParameter("TimeStamp", lEvent.TimeStamp));
+                    cmd.Parameters.Add(new SQLiteParameter("LoginAccount", lEvent.LoginAccount));
+                    int j = cmd.ExecuteNonQuery();
+                    if(j==1)
+                    {
+                        //Trace.WriteLine("--新增事件记录：“登录账户:" + lEvent.LoginAccount + "，最近事件日志ID:" + lEvent.ID
+                        //              + "，事件:" + lEvent.Type + "，事件时间:" + lEvent.TimeStamp.ToString() + "”");
+                    }
+                    return j == 1;
+                }
+                //Trace.WriteLine("--更新事件记录：“登录账户:" + lEvent.LoginAccount + "，最近事件日志ID:" + lEvent.ID
+                //                + "，事件:" + lEvent.Type + "，事件时间:" + lEvent.TimeStamp.ToString() + "”");
+                return true;
+            }
+            catch (Exception)
+            {
+                //Do any logging operation here if necessary
+                return false;
+            }
+        }
+    }
+}
